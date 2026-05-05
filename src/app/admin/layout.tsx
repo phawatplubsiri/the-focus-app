283bca79b26682100be98bfb94db5fd3d3043565
Phawat Plubsiri <phawat.plub@gmail.com>
2026-05-05 07:07:00 +0700

    feat: add admin dashboard layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -3,6 +3,8 @@ import prisma from '@/lib/prisma'
 import { redirect } from 'next/navigation'
 import Link from 'next/link'
 
+export const dynamic = 'force-dynamic'
+
 export default async function AdminLayout({
   children,
 }: {
